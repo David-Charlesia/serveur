4f274ea733ae638ae7ad04b16296c1fac2cc74e6
David-Charlesia <pro.charlesia.david@outlook.com>
2020-03-17 13:17:48 +0100

    first commit

---
+++ udpclient.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:55:57 2020
+
+@author: david
+"""
+
---
+++ udpserveur.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 13:16:29 2020
+
+@author: david
+"""
+
