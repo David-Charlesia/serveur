068ec58b27cf4305560ac6f7503c186b92869609
David-Charlesia <pro.charlesia.david@outlook.com>
2020-03-19 12:00:25 +0100

    commentaire

--- tcpserveur.py
+++ tcpserveur.py
@@ -8,11 +8,13 @@ Created on Tue Mar 17 13:16:29 2020
 import socket
 import sys
 
+#définition des paramètres d'entrées
 host = str(sys.argv[1])
 port = int(sys.argv[2])
 
 host, port = (host,port)
 
+#créeation du socket tcp
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind((host,port))
 
@@ -20,17 +22,17 @@ print("serveur demarrer")
 
 while True:
     socket.listen(5)
-    conn, adress=socket.accept()
+    conn, adress=socket.accept() #connexion acceptée
     print("Client connecte")
 
-    data = conn.recv(1024)
-    data = data.decode("utf8")
-    data = "ok : " + data
+    data = conn.recv(1024) #réception du msg
+    data = data.decode("utf8") #décodage du msg
+    data = "ok : " + data #syntaxe demandé
     print(data)
 
     data_to_send="recu \"" + data + "\""
     data=data.encode("utf8")
-    conn.send(data)
+    conn.send(data) #envoie de la confirmation serveur au Client
 
-conn.close()
-socket.close()
+conn.close() #fermeture de la connexion
+socket.close() #fermeture du socket
